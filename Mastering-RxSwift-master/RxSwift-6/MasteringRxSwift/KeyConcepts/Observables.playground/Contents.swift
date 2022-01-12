ca8f58a2d6052d54a732dc23346bfe3ec74d1c1f
JeongChangYong <ckddyd1202@daum.net>
2022-01-12 21:30:13 +0900

    Observables and Observers #1
    Key Concepts

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/KeyConcepts/Observables.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/KeyConcepts/Observables.playground/Contents.swift
@@ -27,6 +27,15 @@ import RxSwift
 /*:
  # Observables
  */
+// #1
+Observable<Int>.create { (observer) -> Disposable in
+    observer.on(.next(0))
+    observer.onNext(1)
+    
+    observer.onCompleted()
+    
+    return Disposables.create()
+}
 
-
-
+// #2
+Observable.from([0, 1])
