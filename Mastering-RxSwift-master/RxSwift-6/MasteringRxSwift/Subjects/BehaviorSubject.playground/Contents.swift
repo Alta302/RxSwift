d2a8db6e810f7d851b0243c08f603df124532327
JeongChangYong <ckddyd1202@daum.net>
2022-01-21 16:11:39 +0900

    Behavior Subject
    Subjects

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/BehaviorSubject.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/BehaviorSubject.playground/Contents.swift
@@ -34,13 +34,23 @@ enum MyError: Error {
    case error
 }
 
+let p = PublishSubject<Int>()
 
+p.subscribe { print("PublishSubject >>", $0) }
+    .disposed(by: disposeBag)
 
+let b = BehaviorSubject<Int>(value: 0)
 
+b.subscribe { print("BehaviorSubject >>", $0) }
+    .disposed(by: disposeBag)
 
+b.onNext(1)
 
+b.subscribe { print("BehaviorSubject2 >>", $0) }
+    .disposed(by: disposeBag)
 
+// b.onCompleted()
+b.onError(MyError.error)
 
-
-
-
+b.subscribe { print("BehaviorSubject3 >>", $0) }
+    .disposed(by: disposeBag)
