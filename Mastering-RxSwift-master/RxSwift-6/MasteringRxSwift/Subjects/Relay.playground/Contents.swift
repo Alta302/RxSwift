bcb510b86dc614766b82fe642db0fa7db584a7be
JeongChangYong <ckddyd1202@daum.net>
2022-01-24 21:05:29 +0900

    Relays (RxSwift 6)
    Subjects

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/Relay.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Subjects/Relay.playground/Contents.swift
@@ -31,9 +31,27 @@ import RxCocoa
 
 let bag = DisposeBag()
 
+let prelay = PublishRelay<Int>()
 
+prelay.subscribe { print("1: \($0)") }
+    .disposed(by: bag)
 
+prelay.accept(1)
 
+let brelay = BehaviorRelay(value: 1)
 
+brelay.accept(2)
 
+brelay.subscribe { print("2: \($0)") }
+    .disposed(by: bag)
 
+brelay.accept(3)
+
+print(brelay.value)
+
+let rrelay = ReplayRelay<Int>.create(bufferSize: 3)
+
+(1...10).forEach { rrelay.accept($0) }
+
+rrelay.subscribe { print("3: \($0)") }
+    .disposed(by: bag)
