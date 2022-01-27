35c3f4ac538287a32334ae40bba23824d78d073e
JeongChangYong <ckddyd1202@daum.net>
2022-01-27 21:26:12 +0900

    from
    Create Operators

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CreatingOperators/from.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CreatingOperators/from.playground/Contents.swift
@@ -31,12 +31,6 @@ import RxSwift
 let disposeBag = DisposeBag()
 let fruits = ["🍏", "🍎", "🍋", "🍓", "🍇"]
 
-
-
-
-
-
-
-
-
-
+Observable.from(fruits)
+    .subscribe { element in print(element) }
+    .disposed(by: disposeBag)
