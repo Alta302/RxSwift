7521d26bd3c5a0f6d4771f6f057bd6cb85cf9ecc
JeongChangYong <ckddyd1202@daum.net>
2022-02-04 11:07:37 +0900

    create
    Create Operators

--- Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CreatingOperators/create.playground/Contents.swift
+++ Mastering-RxSwift-master/RxSwift-6/MasteringRxSwift/Operators/CreatingOperators/create.playground/Contents.swift
@@ -34,6 +34,23 @@ enum MyError: Error {
    case error
 }
 
-
-
-
+Observable<String>.create { (observer) -> Disposable in
+    guard let url = URL(string: "https://www.apple.com") else {
+        observer.onError(MyError.error)
+        return Disposables.create()
+    }
+    
+    guard let html = try? String(contentsOf: url, encoding: .utf8) else {
+        observer.onError(MyError.error)
+        return Disposables.create()
+    }
+    
+    observer.onNext(html)
+    observer.onCompleted()
+    
+    observer.onNext("After Completed")
+    
+    return Disposables.create()
+}
+    .subscribe { print($0) }
+    .disposed(by: disposeBag)
